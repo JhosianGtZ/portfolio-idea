1bf135dd8d8cb30196dd77255811c5bb4662f9a3
Jhosian <40299796+JhosianGtZ@users.noreply.github.com>
2021-10-16 11:02:37 -0500

    First commit -project at 70%

---
+++ js/app.js
@@ -0,0 +1,16 @@
+function changeBackground() {
+
+    var nav__menu = document.getElementById("menu-nav");
+    var scrollValue = window.scrollY;
+
+
+    if (scrollValue < 20) {
+        nav__menu.classList.remove('menu-bg');
+    } else {
+        nav__menu.classList.add('menu-bg');
+    }
+}
+
+
+
+window.addEventListener('scroll', changeBackground);
